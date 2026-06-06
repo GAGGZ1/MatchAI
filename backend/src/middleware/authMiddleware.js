6a258ec49011130ec344147df273e3d14124e3e7
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2026-06-07 03:38:36 +0530

    improved authmiddleware

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,27 +1,23 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token =
-    req.headers.authorization?.split(" ")[1];
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({
-      message: "Unauthorized"
+      message: "Unauthorized",
     });
   }
 
   try {
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET
-    );
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = decoded;
 
     next();
   } catch {
     return res.status(401).json({
-      message: "Invalid token"
+      message: "Invalid token",
     });
   }
 };
