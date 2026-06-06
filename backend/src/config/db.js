27021fb0f3db2997669f210dc1564a7021f51bff
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2026-06-07 05:13:01 +0530

    added logs for debug

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,32 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    console.log(
+      "MONGO_URI EXISTS:",
+      !!process.env.MONGO_URI
+    );
 
-    console.log("MongoDB Connected");
+    console.log(
+      "MONGO_URI STARTS WITH:",
+      process.env.MONGO_URI?.slice(0, 20)
+    );
+
+    await mongoose.connect(
+      process.env.MONGO_URI
+    );
+
+    console.log(
+      "✅ MongoDB Connected"
+    );
+
   } catch (error) {
-    console.log(error.message);
+
+    console.error(
+      "❌ Mongo Error:"
+    );
+
+    console.error(error);
+
     process.exit(1);
   }
 };
